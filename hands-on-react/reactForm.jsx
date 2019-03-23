1407f744698fecca3e0d3e4153b5d67382d0cde5
M. Leon Martin <13058234+Alquimista3301@users.noreply.github.com>
2019-03-23 14:31:15 -0600

    Create reactForm.jsx

---
+++ hands-on-react/reactForm.jsx
@@ -0,0 +1,10 @@
+// this is an exercice to learn how is the used of States, props, changes metods, and data uses. 
+
+import React from 'react'
+
+class myForm extends React.Component {
+
+  render() {}
+}
+
+export { myForm }
